95697932eb903ada0c9b5564ad417567bb3c4a44
michaelhel <michael.helc.reg@gmail.com>
2019-02-05 09:06:05 +0100

    Added config.js to use instead of a json file

---
+++ Franklyn/app/config/config.js
@@ -0,0 +1,35 @@
+//Configurations
+
+/**
+ * Returns the port
+ */
+function getPort() {
+  return 5005;
+}
+
+/**
+ * Returns the address
+ */
+function getAddress() {
+  return 'franklyn.htl-leonding.ac.at';
+}
+
+/**
+ * Returns the Protocol (http, https)
+ */
+function getProtocol() {
+  return 'http';
+}
+
+/**
+ * Returns the full url with an "/" at the end
+ */
+function getFullUrl() {
+  return getProtocol() + '://' + getAddress() + ':' + getPort() + '/';
+}
+
+
+module.exports.getPort = getPort;
+module.exports.getAddress = getAddress;
+module.exports.getProtocol = getProtocol;
+module.exports.getFullUrl = getFullUrl;
