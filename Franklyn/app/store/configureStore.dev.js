10432911cc42b29830cd0ae8ca68b402a759e0ea
hannesgerstmayr <32167810+hannesgerstmayr@users.noreply.github.com>
2019-11-19 15:43:03 +0100

    refactored code

--- Franklyn/app/store/configureStore.dev.js
+++ Franklyn/app/store/configureStore.dev.js
@@ -4,7 +4,6 @@ import { createHashHistory } from 'history';
 import { routerMiddleware, routerActions } from 'connected-react-router';
 import { createLogger } from 'redux-logger';
 import createRootReducer from '../reducers';
-import * as counterActions from '../actions/counter';
 import type { counterStateType } from '../reducers/types';
 
 const history = createHashHistory();
@@ -36,16 +35,15 @@ const configureStore = (initialState?: counterStateType) => {
 
   // Redux DevTools Configuration
   const actionCreators = {
-    ...counterActions,
     ...routerActions
   };
   // If Redux DevTools Extension is installed use it, otherwise use Redux compose
   /* eslint-disable no-underscore-dangle */
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        // Options: http://extension.remotedev.io/docs/API/Arguments.html
-        actionCreators
-      })
+      // Options: http://extension.remotedev.io/docs/API/Arguments.html
+      actionCreators
+    })
     : compose;
   /* eslint-enable no-underscore-dangle */
 
